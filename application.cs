7e1034556c9a3b8c50cd5dd677837516da5f7fb8
Kenya Chan <kenya.chan@gmail.com>
2021-03-10 00:03:33 +1100

    Add Download CV method
    Moved the download CV method from ApplicationDownloader to Application
    class.

--- application.cs
+++ application.cs
@@ -1,6 +1,8 @@
 namespace Download_Applications
 {
     using System;
+    using System.Diagnostics;
+    using System.IO;
 
     public class Application
     {
@@ -25,5 +27,44 @@ namespace Download_Applications
         public string CvName { get => _cvName; set => _cvName = value; }
         public string ScLink { get => _scLink; set => _scLink = value; }
         public string ScName { get => _scName; set => _scName = value; }
+
+        public Application()
+        {
+
+        }
+
+        public bool DownloadCV(String downloadFolderPath)
+        {
+            if (this.CvLink.Equals(String.Empty))
+            {
+                Console.WriteLine($"No CV attached for applicant {this.FirstName} {this.LastName}");
+                return false;
+            }
+
+            Console.WriteLine($"Downloading {this.CvName}");
+            // navigates to url (downloads the application)
+            String uri = this.CvLink;
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.UseShellExecute = true;
+            psi.FileName = uri;
+            Process.Start(psi);
+
+            // Check if file is downloaded
+            DateTime timeout = DateTime.Now.AddSeconds(20);
+
+            while (DateTime.Now <= timeout)
+            {
+                if (File.Exists($"{downloadFolderPath}/{this.CvName}"))
+                {
+                    Console.WriteLine($"File downloaded : {downloadFolderPath}/{this.CvName}");
+                    return true;
+                }
+
+                System.Threading.Thread.Sleep(200);
+            }
+
+            Console.WriteLine($"File failed to download : {this.CvName}");
+            return false;
+        }
     }
 }
